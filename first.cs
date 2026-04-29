ae985ba97be1608f95f748abc45c757049c0bb2a
Bharat Rathore <omprathore55555@gmail.com>
2026-04-29 13:54:01 +0530

    first C# Code

---
+++ first.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class bharat
+{
+    static void Main()
+    {
+        // Console.WriteLine("My Name is Bharat Choudhary");
+
+        int a = 30;
+        int b = 30;
+
+        Console.WriteLine("Total Sum of : "+ (a+b));
+        Console.WriteLine($"Total Sum Is : {a + b}");
+    }
+}
